e2ff4d6244deb5f3cc01442a7c6d46d19c63886c
cindee-san <49595063+cindee-san@users.noreply.github.com>
2022-06-22 13:25:36 -0700

    wrote some of the classes Phrase and Game

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,18 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor(missed, phrases, activePhrase){
+        this.missed = 0,
+        this.phrases = //remove numbers and special characters
+        this.activePhrase = //think this through
+    }
+    get startGame() //hides start screen overlay, calls getRandomPhrase method, sets activePhrase property and adds to board using activePhrase.addPhraseToDisplay
+    get getRandomPhrase()
+    get handleInteraction()
+    get removeLife()
+    get checkForWin()
+    get gameOver() //displays original start screen overlay, updates overlay h1 with win  or loss message, replaces overlay's start css class with either win or lose css class
+
+}
